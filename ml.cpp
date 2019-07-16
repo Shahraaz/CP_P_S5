e2c1bf54260709d241b7836e1e0f9f8ad8836dba
Shahraaz Hussain <shahraazhussain@gmail.com>
2019-07-16 05:31:08 +0530

    Initial

---
+++ ml.cpp
@@ -0,0 +1,72 @@
+https://developers.google.com/machine-learning/crash-course/descending-into-ml/video-lecture
+
+http://www.cs.cmu.edu/~ninamf/courses/601sp15/lectures.shtml
+
+https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0002-introduction-to-computational-thinking-and-data-science-fall-2016/lecture-videos/
+
+http://videolectures.net/andrew_ng/
+
+https://freevideolectures.com/course/2272/artificial-intelligence
+
+http://ai.berkeley.edu/lecture_videos.html
+
+https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-034-artificial-intelligence-fall-2010/lecture-videos/
+
+https://www.manning.com/livevideo/grokking-deep-learning-in-motion?a_aid=algmotion&a_bid=5d7bc0ba
+
+https://in.udacity.com/course/intro-to-artificial-intelligence--cs271
+
+https://in.udacity.com/course/artificial-intelligence-for-robotics--cs373
+
+https://in.udacity.com/course/deep-learning--ud730-india
+
+http://videolectures.net/mlss09uk_bishop_ibi/?q=bishop
+
+https://sites.google.com/view/deep-rl-bootcamp/lectures
+
+https://in.udacity.com/course/knowledge-based-ai-cognitive-systems--ud409
+
+Data Science
+https://nptel.ac.in/courses/106106179/#
+
+AI
+https://nptel.ac.in/courses/106106140/
+
+Big Data Computing
+https://nptel.ac.in/courses/106104189/
+
+Deep Learning
+https://nptel.ac.in/courses/106106184/
+
+Deep Learning 2
+https://nptel.ac.in/courses/106106201/
+
+Data Mining
+https://nptel.ac.in/courses/106105174/
+
+Data Science 
+https://nptel.ac.in/courses/106106179/
+
+Intro to ML
+https://nptel.ac.in/courses/106105152/
+
+INtro to ML(Articent)
+https://nptel.ac.in/courses/106106139/
+
+INtro to ML(Articent)
+https://nptel.ac.in/courses/106106198/
+
+ML
+https://nptel.ac.in/courses/106106202/
+
+AI(Dasgupta)
+https://nptel.ac.in/courses/106105079/
+
+AI
+https://nptel.ac.in/courses/106106126/
+
+Algorithm For Big Data
+https://nptel.ac.in/courses/106106142/
+
+DP Approx
+https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-231-dynamic-programming-and-stochastic-control-fall-2015/related-video-lectures/
