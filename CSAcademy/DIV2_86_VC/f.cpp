1788b0189ea4d11667957ad649e79e55623804b6
Shahraaz Hussain <ShahraazHussain@gmail.com>
2019-12-04 12:00:18 +0530

    Wed, Dec 04 12:00 AutoCommit

--- CSAcademy/DIV2_86_VC/f.cpp
+++ CSAcademy/DIV2_86_VC/f.cpp
@@ -1,90 +1,203 @@
-//Optimise
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <cstring>
+#include <set>
+#include <algorithm>
+
 using namespace std;
-using namespace __gnu_pbds;
 
-// #define MULTI_TEST
-#ifdef LOCAL
-#define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__);
-#define pc(...) PC(#__VA_ARGS__, __VA_ARGS__);
-template <typename T, typename U>
-ostream &operator<<(ostream &out, const pair<T, U> &p)
-{
-    out << '[' << p.first << ", " << p.second << ']';
-    return out;
-}
-template <typename Arg>
-void PC(const char *name, Arg &&arg)
-{
-    while (*name == ',' || *name == ' ')
-        name++;
-    std::cerr << name << " { ";
-    for (const auto &v : arg)
-        cerr << v << ' ';
-    cerr << " }\n";
-}
-template <typename Arg1, typename... Args>
-void PC(const char *names, Arg1 &&arg1, Args &&... args)
-{
-    while (*names == ',' || *names == ' ')
-        names++;
-    const char *comma = strchr(names, ',');
-    std::cerr.write(names, comma - names) << " { ";
-    for (const auto &v : arg1)
-        cerr << v << ' ';
-    cerr << " }\n";
-    PC(comma, args...);
-}
-template <typename Arg1>
-void ZZ(const char *name, Arg1 &&arg1)
-{
-    std::cerr << name << " = " << arg1 << endl;
-}
-template <typename Arg1, typename... Args>
-void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
-{
-    const char *comma = strchr(names + 1, ',');
-    std::cerr.write(names, comma - names) << " = " << arg1;
-    ZZ(comma, args...);
-}
-#else
-#define db(...)
-#define pc(...)
-#endif
+const int NMAX = 1e5;
 
-using ll = long long;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-#define f first
-#define s second
-#define pb push_back
-auto TimeStart = chrono::steady_clock::now();
+struct event_t{
+	int len;
+	int id;
+	bool is_query;
 
-const int NAX = 2e5 + 10, MOD = 1000000007;
+	event_t(){
+		len = 0;
+		id = 0;
+		is_query = 0;
+	}
 
-void solveCase(int caseNo)
-{
-}
+	event_t(int len,int id,bool is_query){
+		this->len = len;
+		this->id = id;
+		this->is_query = is_query;
+	}
 
-int main()
-{
-#ifndef LOCAL
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-#endif
-    int t = 1;
-#ifdef MULTI_TEST
-    cin >> t;
-#endif
-    for (int i = 1; i <= t; ++i)
-    {
-        solveCase(i);
-#ifdef TIME
-        cerr << "Case #" << i << ": Time " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " s.\n";
-        TimeStart = chrono::steady_clock::now();
-#endif
-    }
+	bool operator < (const event_t &other)const{
+		if(len != other.len){
+			return len < other.len;
+		}
+		if(id != other.id){
+			return id < other.id;
+		}
+		return is_query < other.is_query;
+	}
+};
+
+class DSU{
+public:
+	int father[NMAX + 5];
+	int sz[NMAX + 5];
+	int weight[NMAX + 5];
+	int last_element[NMAX + 5];
+
+	DSU(){
+		memset(father,0,sizeof(father));
+		memset(sz,0,sizeof(sz));
+		memset(weight,0,sizeof(weight));
+		memset(last_element,0,sizeof(last_element));
+
+		for(int i = 1;i <= NMAX;i++){
+			sz[i] = 1;
+			last_element[i] = i;
+			weight[i] = 1;
+		}
+	}
+
+	int get_root(int a){
+		if(father[a] == 0){
+			return a;
+		}
+		return father[a] = get_root(father[a]);
+	}
+
+	void join(int a,int b){
+		a = get_root(a);
+		b = get_root(b);
+		if(sz[a] < sz[b]){
+			sz[b] += sz[a];
+			father[a] = b;
+			weight[b] = min(weight[a],weight[b]);
+			last_element[b] = max(last_element[a],last_element[b]);
+		}
+		else{
+			sz[a] += sz[b];
+			father[b] = a;
+			weight[a] = min(weight[a],weight[b]);
+			last_element[a] = max(last_element[a],last_element[b]);
+		}
+	}
+
+	int get_size(int a){
+		return sz[get_root(a)];
+	}
+
+	int get_weight(int a){
+		return weight[get_root(a)] && get_size(a) % 2 == 1;
+	}
+
+	int get_last_element(int a){
+		return last_element[get_root(a)];
+	}
+};
+
+int n,q;
+int points[NMAX + 5];
+int lengths[NMAX + 5];
+DSU active_lengths;
+set<event_t> what_is_happening;
+long long ans[NMAX + 5];
+int last_timeout[NMAX + 5];
+bool timedout[NMAX + 5];
+int remaining_queries;
+
+int main() {
+
+	fscanf(stdin,"%d %d",&n,&q);
+
+	for(int i = 1;i <= n;i++){
+		fscanf(stdin,"%d",&points[i]);
+	}
+
+	int sz = 1;
+
+	for(int i = 2;i <= n;i++){
+		if(points[sz - 1] <= points[sz] && points[sz] <= points[i]){
+			points[sz] = points[i];
+		}
+
+		else if(points[sz - 1] >= points[sz] && points[sz] >= points[i]){
+			points[sz] = points[i];
+		}
+
+		else{
+			points[++sz] = points[i];
+		}
+	}
+
+	long long initial_cost = 0;
+	int initial_len = 0;
+    int delta = sz;
+
+	for(int i = 1;i <= sz;i++){
+		lengths[i] = abs(points[i] - points[i - 1]);
+		initial_cost += lengths[i];
+	}
+
+	if(points[1] < 0){
+		for(int i = 1;i <= sz;i++){
+			lengths[i] = lengths[i + 1];
+		}
+		sz--;
+		delta--;
+	}
+
+	for(int i = 1;i <= sz;i++){
+		what_is_happening.insert(event_t(lengths[i],i,0));
+	}
+
+	for(int i = 1;i <= q;i++){
+		int len;
+		fscanf(stdin,"%d",&len);
+		what_is_happening.insert(event_t(len,i,1));
+	}
+
+	remaining_queries = q;
+
+	while(remaining_queries){
+		event_t event = *what_is_happening.begin();
+		what_is_happening.erase(what_is_happening.begin());
+
+		initial_cost -= 1LL * delta * (event.len - initial_len);
+		initial_len = event.len;
+
+		if(event.is_query){
+			ans[event.id] = initial_cost;
+			remaining_queries--;
+		}
+		else{
+			delta -= active_lengths.get_weight(event.id);
+
+			if(event.id < sz){
+				delta -= active_lengths.get_weight(event.id + 1);
+				active_lengths.join(event.id,event.id + 1);
+				delta += active_lengths.get_weight(event.id);
+				if(active_lengths.get_size(event.id) % 2 == 0){
+					if(!timedout[active_lengths.get_last_element(event.id)]){
+						last_timeout[active_lengths.get_last_element(event.id)] = event.len;
+						timedout[active_lengths.get_last_element(event.id)] = 1;
+						what_is_happening.erase(event_t(lengths[active_lengths.get_last_element(event.id)],active_lengths.get_last_element(event.id),0));
+					}
+				}
+				else{
+					if(timedout[active_lengths.get_last_element(event.id)]){
+						lengths[active_lengths.get_last_element(event.id)] += event.len - last_timeout[active_lengths.get_last_element(event.id)];
+						timedout[active_lengths.get_last_element(event.id)] = 0;
+						what_is_happening.insert(event_t(lengths[active_lengths.get_last_element(event.id)],active_lengths.get_last_element(event.id),0));
+					}
+				}
+			}
+			else{
+				active_lengths.weight[active_lengths.get_root(event.id)];
+			}
+
+		}
+	}
+
+	for(int i = 1;i <= q;i++){
+		fprintf(stdout,"%lld\n",ans[i]);
+	}
+
     return 0;
 }
