e1dff528bc924e5e70b38a149eb42ea11a19b46d
Shahraaz Hussain <ShahraazHussain@gmail.com>
2020-01-02 19:18:26 +0530

    Thu, Jan 02 19:18 AutoCommit

--- CODENATION/a.cpp
+++ CODENATION/a.cpp
@@ -8,14 +8,14 @@ using namespace std;
 template <typename Arg1>
 void ZZ(const char *name, Arg1 &&arg1)
 {
-	std::cerr << name << " = " << arg1 << endl;
+    std::cerr << name << " = " << arg1 << endl;
 }
 template <typename Arg1, typename... Args>
 void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
 {
-	const char *comma = strchr(names + 1, ',');
-	std::cerr.write(names, comma - names) << " = " << arg1;
-	ZZ(comma, args...);
+    const char *comma = strchr(names + 1, ',');
+    std::cerr.write(names, comma - names) << " = " << arg1;
+    ZZ(comma, args...);
 }
 #else
 #define db(...)
@@ -32,54 +32,54 @@ const int nax = 2e5 + 10;
 
 void solve()
 {
-	int n, m, x;
-	cin >> n >> m >> x;
-	vector<vector<int>> Adj(n + 1), indegree(n + 1);
-	int u, v;
-	for (int i = 0; i < m; ++i)
-	{
-		cin >> u >> v;
-		Adj[u].pb(v);
-		indegree[v]++;
-	}
-	vector<int> Oddways(n + 1), Evenways(n + 1);
-	queue<int> Q;
-	for (int i = 1; i <= n; ++i)
-		if (!indegree[i])
-			Q, push(i);
-	Oddways[x] = 1;
-	while (!Q.empty())
-	{
-		auto top = Q.front();
-		Q.pop();
-		for (int child : Adj[top])
-		{
-			Oddways[child] += Evenways[top];
-			Evenways[child] += Oddways[top];
-			Oddways[child] %= mod;
-			Evenways[child] %= mod;
-			indegree[child]--;
-			if (!indegree[child])
-				Q.push(child);
-		}
-	}
-	for (int i = 1; i <= n; ++i)
-		cout << Evenways[i] << ' ';
-	cout << '\n';
+    int n, m, x;
+    cin >> n >> m >> x;
+    vector<vector<int>> Adj(n + 1), indegree(n + 1);
+    int u, v;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> u >> v;
+        Adj[u].pb(v);
+        indegree[v]++;
+    }
+    vector<int> Oddways(n + 1), Evenways(n + 1);
+    queue<int> Q;
+    for (int i = 1; i <= n; ++i)
+        if (!indegree[i])
+            Q.push(i);
+    Oddways[x] = 1;
+    while (!Q.empty())
+    {
+        auto top = Q.front();
+        Q.pop();
+        for (int child : Adj[top])
+        {
+            Oddways[child] += Evenways[top];
+            Evenways[child] += Oddways[top];
+            Oddways[child] %= mod;
+            Evenways[child] %= mod;
+            indegree[child]--;
+            if (!indegree[child])
+                Q.push(child);
+        }
+    }
+    for (int i = 1; i <= n; ++i)
+        cout << Evenways[i] << ' ';
+    cout << '\n';
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int t = 1;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
 #ifdef multitest
-	cin >> t;
+    cin >> t;
 #endif
-	while (t--)
-		solve();
+    while (t--)
+        solve();
 #ifdef TIME
-	cerr << "\n\nTime elapsed: " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " seconds.\n";
+    cerr << "\n\nTime elapsed: " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " seconds.\n";
 #endif
-	return 0;
+    return 0;
 }
