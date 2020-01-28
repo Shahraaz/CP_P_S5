ff8c5ff103bdc4a015eaeee1b78f473fd4787ba4
Shahraaz Hussain <shahraazhussain@gmail.com>
2020-01-28 14:50:33 +0530

    Uber Mock Online ALL AC

---
+++ Leetcode/Mock_Uber_Online/a.cpp
@@ -0,0 +1,117 @@
+// Optimise
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+// #define MULTI_TEST
+#ifdef LOCAL
+#define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__);
+#define pc(...) PC(#__VA_ARGS__, __VA_ARGS__);
+template <typename T, typename U>
+ostream &operator<<(ostream &out, const pair<T, U> &p)
+{
+    out << '[' << p.first << ", " << p.second << ']';
+    return out;
+}
+template <typename Arg>
+void PC(const char *name, Arg &&arg)
+{
+    while (*name == ',' || *name == ' ')
+        name++;
+    std::cerr << name << " { ";
+    for (const auto &v : arg)
+        cerr << v << ' ';
+    cerr << " }\n";
+}
+template <typename Arg1, typename... Args>
+void PC(const char *names, Arg1 &&arg1, Args &&... args)
+{
+    while (*names == ',' || *names == ' ')
+        names++;
+    const char *comma = strchr(names, ',');
+    std::cerr.write(names, comma - names) << " { ";
+    for (const auto &v : arg1)
+        cerr << v << ' ';
+    cerr << " }\n";
+    PC(comma, args...);
+}
+template <typename Arg1>
+void ZZ(const char *name, Arg1 &&arg1)
+{
+    std::cerr << name << " = " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
+{
+    const char *comma = strchr(names + 1, ',');
+    std::cerr.write(names, comma - names) << " = " << arg1;
+    ZZ(comma, args...);
+}
+#else
+#define db(...)
+#define pc(...)
+#endif
+
+using ll = long long;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define f first
+#define s second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+auto TimeStart = chrono::steady_clock::now();
+auto seed = TimeStart.time_since_epoch().count();
+std::mt19937 rng(seed);
+template <typename T>
+using Random = std::uniform_int_distribution<T>;
+
+const int NAX = 2e5 + 5, MOD = 1000000007;
+
+class Solution {
+public:
+    int shortestDistance(vector<string>& words, string word1, string word2) {
+        map<string,vector<int>> mappin;
+        int res = INT_MAX;
+        for (int i = 0; i < words.size(); i++)
+            mappin[words[i]].pb(i);
+        for (auto &elem : mappin[word1])
+        {
+            auto it = lower_bound(all(mappin[word2]),elem);
+            if(it != mappin[word2].end())
+            {
+                res = min(res,*it-elem+1);
+            }
+            if(it != mappin[word2].begin())
+            {
+                --it;
+                res = min(res,elem-*it+1);
+            }
+        }
+        
+        return res;
+    }
+};
+
+#ifdef LOCAL
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+#ifdef MULTI_TEST
+    cin >> t;
+#endif
+    Solution S;
+    for (int i = 1; i <= t; ++i)
+    {
+        ;
+#ifdef TIME
+        cerr << "Case #" << i << ": Time " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " s.\n";
+        TimeStart = chrono::steady_clock::now();
+#endif
+    }
+    return 0;
+}
+#endif
