06dbd7df8eb7bef6a3eb698690748a2717cf8e23
Mohammed Shahraaz Hussain <shahraazhussain@gmail.com>
2020-02-05 21:55:03 +0530

    Wed, Feb 05 21:55 CodeOverFlow 1.1 Sols

---
+++ Atcoder/AGC_037_VC/b.cpp
@@ -0,0 +1,135 @@
+// Optimise
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+// #define MULTI_TEST
+#ifdef LOCAL
+#define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__);
+#define pc(...) PC(#__VA_ARGS__, __VA_ARGS__);
+template <typename T, typename U>
+ostream &operator<<(ostream &out, const pair<T, U> &p)
+{
+    out << '[' << p.first << ", " << p.second << ']';
+    return out;
+}
+template <typename Arg>
+void PC(const char *name, Arg &&arg)
+{
+    while (*name == ',' || *name == ' ')
+        name++;
+    std::cerr << name << " { ";
+    for (const auto &v : arg)
+        cerr << v << ' ';
+    cerr << " }\n";
+}
+template <typename Arg1, typename... Args>
+void PC(const char *names, Arg1 &&arg1, Args &&... args)
+{
+    while (*names == ',' || *names == ' ')
+        names++;
+    const char *comma = strchr(names, ',');
+    std::cerr.write(names, comma - names) << " { ";
+    for (const auto &v : arg1)
+        cerr << v << ' ';
+    cerr << " }\n";
+    PC(comma, args...);
+}
+template <typename Arg1>
+void ZZ(const char *name, Arg1 &&arg1)
+{
+    std::cerr << name << " = " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
+{
+    const char *comma = strchr(names + 1, ',');
+    std::cerr.write(names, comma - names) << " = " << arg1;
+    ZZ(comma, args...);
+}
+#else
+#define db(...)
+#define pc(...)
+#endif
+
+using ll = long long;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define f first
+#define s second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+auto TimeStart = chrono::steady_clock::now();
+auto seed = TimeStart.time_since_epoch().count();
+std::mt19937 rng(seed);
+template <typename T>
+using Random = std::uniform_int_distribution<T>;
+
+const int NAX = 2e5 + 5, MOD = 1000000007;
+
+void solveCase()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    priority_queue<pair<int, int>> q;
+    for (size_t i = 0; i < n; i++)
+        cin >> a[i];
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> b[i];
+        q.push({b[i], i});
+    }
+    ll cnt = 0;
+    while (!q.empty())
+    {
+        int u = q.top().s;
+        q.pop();
+        int prev = (u - 1 + n) % n, next = (u + 1) % n;
+        if (a[u] == b[u])
+            continue;
+        if (b[u] < a[u])
+        {
+            cout << "-1\n";
+            return;
+        }
+        ll bplusc = b[prev] + b[next];
+        if (a[u] >= b[prev] && a[u] >= b[next])
+        {
+            if ((b[u] - a[u]) % bplusc == 0)
+                cnt += (b[u] - a[u]) / bplusc;
+            else
+            {
+                cout << "-1\n";
+                return;
+            }
+            b[u] = a[u];
+        }
+        else
+        {
+        }
+    }
+}
+
+int32_t main()
+{
+#ifndef LOCAL
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+#endif
+    int t = 1;
+#ifdef MULTI_TEST
+    cin >> t;
+#endif
+    for (int i = 1; i <= t; ++i)
+    {
+        solveCase();
+#ifdef TIME
+        cerr << "Case #" << i << ": Time " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " s.\n";
+        TimeStart = chrono::steady_clock::now();
+#endif
+    }
+    return 0;
+}
