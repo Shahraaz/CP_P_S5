6e01d08fa6fedcdf03ac5bece330de8f4f9ac04e
Mohammed Shahraaz Hussain <shahraazhussain@gmail.com>
2020-02-29 12:06:16 +0530

    Saturday 29 February 2020 12:06:16 PM IST AutoCommit

--- ICPC_Mirrors/Nitc_1.0/i.cpp
+++ ICPC_Mirrors/Nitc_1.0/i.cpp
@@ -1,103 +1,93 @@
-// Optimise
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <set>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <algorithm>
+#include <memory.h>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+#include <sstream>
+ 
 using namespace std;
-using namespace __gnu_pbds;
-
-// #define MULTI_TEST
-#ifdef LOCAL
-#define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__);
-#define pc(...) PC(#__VA_ARGS__, __VA_ARGS__);
-template <typename T, typename U>
-ostream &operator<<(ostream &out, const pair<T, U> &p)
-{
-    out << '[' << p.first << ", " << p.second << ']';
-    return out;
+ 
+int P, V;
+ 
+bool query(int r) {
+  printf("check 0 %d\n", r);
+  fflush(stdout);
+  char foo[42];
+  scanf("%s", foo);
+  return (foo[0] == 'Y');
 }
-template <typename Arg>
-void PC(const char *name, Arg &&arg)
-{
-    while (*name == ',' || *name == ' ')
-        name++;
-    std::cerr << name << " { ";
-    for (const auto &v : arg)
-        cerr << v << ' ';
-    cerr << " }\n";
+ 
+void answer(int r) {
+  printf("answer %d\n", r);
+  fflush(stdout);
 }
-template <typename Arg1, typename... Args>
-void PC(const char *names, Arg1 &&arg1, Args &&... args)
-{
-    while (*names == ',' || *names == ' ')
-        names++;
-    const char *comma = strchr(names, ',');
-    std::cerr.write(names, comma - names) << " { ";
-    for (const auto &v : arg1)
-        cerr << v << ' ';
-    cerr << " }\n";
-    PC(comma, args...);
-}
-template <typename Arg1>
-void ZZ(const char *name, Arg1 &&arg1)
-{
-    std::cerr << name << " = " << arg1 << endl;
-}
-template <typename Arg1, typename... Args>
-void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
-{
-    const char *comma = strchr(names + 1, ',');
-    std::cerr.write(names, comma - names) << " = " << arg1;
-    ZZ(comma, args...);
-}
-#else
-#define db(...)
-#define pc(...)
-#endif
-
-using ll = long long;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-#define f first
-#define s second
-#define pb push_back
-#define all(v) v.begin(), v.end()
-auto TimeStart = chrono::steady_clock::now();
-auto seed = TimeStart.time_since_epoch().count();
-std::mt19937 rng(seed);
-template <typename T>
-using Random = std::uniform_int_distribution<T>;
-
-const int NAX = 2e5 + 5, MOD = 1000000007;
-
-class Solution
-{
-private:
-public:
-    Solution() {}
-    ~Solution() {}
-    void solveCase()
-    {
+ 
+const int N = 123456;
+ 
+int va[N], vb[N], vc[N];
+ 
+long long build(int it, int c) {
+  long long cur = 0;
+  for (int p = 0; p <= P; p++) {
+    if (va[p] > vb[p]) {
+      continue;
     }
-};
-
-int32_t main()
-{
-#ifndef LOCAL
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-#endif
-    int t = 1;
-#ifdef MULTI_TEST
-    cin >> t;
-#endif
-    Solution mySolver;
-    for (int i = 1; i <= t; ++i)
-    {
-        mySolver.solveCase();
-#ifdef TIME
-        cerr << "Case #" << i << ": Time " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " s.\n";
-        TimeStart = chrono::steady_clock::now();
-#endif
+    int mv = c - p;
+    if (it == 0) mv = (mv >= 0) ? V : -1; else
+    if (mv < 0) mv = -1;
+    else mv /= it;
+    vc[p] = mv;
+    if (mv >= vb[p]) cur += vb[p] - va[p] + 1; else
+    if (mv >= va[p]) cur += mv - va[p] + 1;
+  }
+  return cur;
+}
+ 
+int main() {
+  scanf("%d %d", &P, &V);
+  for (int p = 0; p <= P; p++) {
+    va[p] = 0;
+    vb[p] = V;
+  }
+  long long total = 1LL * (P + 1) * (V + 1);
+  for (int it = 0; it <= 99; it++) {
+    int a = 0, b = P + it * V;
+    while (a < b) {
+      int c = (a + b) >> 1;
+      long long cur = build(it, c);
+      if (2 * cur >= total) b = c;
+      else a = c + 1;
     }
-    return 0;
+    build(it, a);
+    bool res = query(a);
+    if (res) {
+      for (int p = 0; p <= P; p++) {
+        if (vc[p] < vb[p]) vb[p] = vc[p];
+      }
+    } else {
+      for (int p = 0; p <= P; p++) {
+        if (vc[p] + 1 > va[p]) va[p] = vc[p] + 1;
+      }
+    }
+    total = 0;
+    for (int p = 0; p <= P; p++) {
+      if (va[p] <= vb[p]) total += vb[p] - va[p] + 1;
+    }
+    if (total == 1) {
+      for (int p = 0; p <= P; p++)
+        if (va[p] <= vb[p]) answer(p + (it + 1) * va[p]);
+      break;
+    }
+  }
+  return 0;
 }
